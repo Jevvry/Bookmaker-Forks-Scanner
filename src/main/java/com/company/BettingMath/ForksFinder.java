fcddf01ffb6d64e65fdd26477201cae3da507a55
Ilya_Shumilin <i.sh-vk@yandex.ru>
2020-10-05 21:58:36 +0500

    Создание новой ветки

--- src/main/java/com/company/BettingMath/ForksFinder.java
+++ src/main/java/com/company/BettingMath/ForksFinder.java
@@ -1,10 +1,25 @@
 package com.company.BettingMath;
 
 import com.company.BettingDescription.BetOffice;
+import com.company.BettingDescription.Fork;
 
 import java.util.ArrayList;
 
 public class ForksFinder {
+    //Length: 2
     private ArrayList<BetOffice> offices;
+    private ArrayList<Fork> forks;
+
+    public ForksFinder() {
+        offices = new ArrayList<>();
+        forks = new ArrayList<>();
+    }
+
+    public ArrayList<Fork> FindForks()
+    {
+        BetOffice firstEvents = offices.get(0);
+        BetOffice second = offices.get(1);
+        return  null;
+    }
 
 }
